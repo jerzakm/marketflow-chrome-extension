542f27a8d53b5535d77cea2585506dcaafe33cfd
Marcin <jerzakm@gmail.com>
2019-11-25 23:54:09 +0100

    initial boilerplate commit

---
+++ src/content_script.ts
@@ -0,0 +1,17 @@
+
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+    if (msg.color) {
+        console.log('Receive color = ' + msg.color);
+        document.body.style.backgroundColor = msg.color;
+        sendResponse('Change color to ' + msg.color);
+    } else {
+        sendResponse('Color message is none.');
+    }
+    const a = $.ajax;
+});
+
+const append = () => {
+    alert('appending2')
+}
+
+append()
