3b0f8541baf868ef0de2d96aed54ddac98893417
Marcin <jerzakm@gmail.com>
2019-11-26 01:30:17 +0100

    adds charts.js example on allegro webpage

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,3 +1,4 @@
+import * as Chart from 'chart.js'
 
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     if (msg.color) {
@@ -11,7 +12,53 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
 });
 
 const append = () => {
-    alert('appending2')
+    const element = document.querySelector('[data-box-name="Links-carousel card"]')
+
+    const node = document.createElement('div')
+    node.id = 'marketflow-chrome-extension-container'
+
+    const canvas = document.createElement('canvas')
+    node.appendChild(canvas)
+    element.prepend(node)
+
+    console.log(Chart)
+
+    var myChart = new Chart(canvas, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
 }
 
 append()
